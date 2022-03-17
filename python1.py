55eb95d84cd9fa70d73dec0c9313b01edb5737f5
MOmagdy2074 <101816407+MOmagdy2074@users.noreply.github.com>
2022-03-17 18:08:03 +0200

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,26 @@
+# first task
+
+
+# actor=input("Enter your favourite actor name:")
+# actress=input("Enter your favourite actress name:")
+# 
+# print("Your favourite actor name is", actor, "and your favourite actress name is", actress)
+
+
+
+
+# secound task
+
+# s = "Lucky"
+# print(s)
+
+
+
+# third task
+
+# Day=input("Enter the day:")
+# Month=input("Enter the month:")
+# Year=input("Enter the year:")
+# 
+# print("Today is",Day,"/",Month,"/",Year)
+
